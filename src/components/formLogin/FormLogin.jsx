582e51f25a808283845df2f1c49d6d1cf2cd1cc8
mateok13 <mateoroserok13@gmail.com>
2024-05-22 16:42:30 -0500

    form validation

--- src/components/formLogin/FormLogin.jsx
+++ src/components/formLogin/FormLogin.jsx
@@ -1,26 +1,49 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './FormLogin.css'
 
 function FormLogin() {
+    const [mistakes, setMistakes] = useState({});
+
+    const validateFields = (form) => {
+        let mistakes = {};
+        let regexUserName = /^[A-Za-z0-9]+$/;
+
+        if (!form.userName.trim()) {
+            mistakes.userName = 'The user name field must not be empty, enter your user name';
+        } else if (!regexUserName.test(form.userName)) {
+            mistakes.userName = 'The user name can only contain letters and numbers';
+        }
+
+        if (!form.password.trim()) {
+            mistakes.password = 'The password field must not be empty, enter your password';
+        }
+
+        return mistakes;
+    }
 
     const handleSubmit = (event) => {
         event.preventDefault();
 
-        const form = event.target;
+        const form = new FormData(event.target);
 
-        const userName = form.userName.value;
-        const password = form.password.value;
+        const formData = {};
+        form.forEach((value, key) => {
+            formData[key] = value;
+        });
 
-        console.log('User:', userName);
-        console.log('Password:', password);
+        const mistake = validateFields(formData);
+        setMistakes(mistake);
     }
 
     return (
         <div className='containerLogin'>
             <p className='logo'>No Country Tube</p>
             <form className='formLogin' onSubmit={handleSubmit}>
-                <input className='inputLogin' type="text" placeholder='User name' id='userName' name='userName' required />
-                <input className='inputLogin' type="password" placeholder='Password' id='password' name='password' required />
+                <input className='inputLogin' type="text" placeholder='User name' id='userName' name='userName' />
+                {mistakes.userName && <span className='alertLogin alertUserNameLogin'>{mistakes.userName}</span>}
+                <input className='inputLogin' type="password" placeholder='Password' id='password' name='password' />
+                {mistakes.password && <span className='alertLogin alertPasswordLogin'>{mistakes.password}</span>}
 
                 <button className='buttonLogin' type='submit'>Iniciar sesión</button>
                 <div>
--- src/components/formRegister/FormRegister.jsx
+++ src/components/formRegister/FormRegister.jsx
@@ -1,47 +1,100 @@
 import { useState } from 'react';
-import './FormRegister.css'
+import './FormRegister.css';
 
 function FormRegister() {
     const [date, setDate] = useState('');
 
-    const handleDateChange = (event) => {
-        setDate(event.target.value);
-    };
+    const [mistakes, setMistakes] = useState({});
+
+    const validateFields = (form) => {
+        let mistakes = {};
+        let regexTextOnly = /^[A-Za-záéíóúÁÉÍÓÚüÜñÑ]+(\s[A-Za-záéíóúÁÉÍÓÚüÜñÑ]+)*$/;
+        let regexEmail = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/;
+        let regexUserName = /^[A-Za-z0-9]+$/;
+        let regexPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+        // let regexFecha = /^(19|20)\d{2}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/;
+
+        if (!form.firstName.trim()) {
+            mistakes.firstName = 'The first name field must not be empty, enter your first name';
+        } else if (!regexTextOnly.test(form.firstName)) {
+            mistakes.firstName = 'The first name can only contain letters and spaces (a space between each word)';
+        }
+
+        if (!form.email.trim()) {
+            mistakes.email = 'The email field must not be empty, enter your email';
+          } else if (!regexEmail.test(form.email)) {
+            mistakes.email = 'The email has an invalid format';
+          }
+
+        if (!form.userName.trim()) {
+            mistakes.userName = 'The user name field must not be empty, enter your user name';
+        } else if (!regexUserName.test(form.userName)) {
+            mistakes.userName = 'The user name can only contain letters and numbers';
+        }
+
+        if (!form.password.trim()) {
+            mistakes.password = 'The password field must not be empty, enter your password';
+        } else if(!regexPassword.test(form.password)){
+            mistakes.password = 'The password has an invalid format';
+        }
+
+        if (!form.confirmPassword.trim()) {
+            mistakes.confirmPassword = 'The confirm password field must not be empty, confirm your password';
+        } else if (form.password !== form.confirmPassword) {
+            mistakes.confirmPassword = 'The passwords do not match';
+        }
+
+        return mistakes;
+    }
 
     const handleSubmit = (event) => {
         event.preventDefault();
 
-        const form = event.target;
+        const form = new FormData(event.target);
 
-        const firstName = form.firstName.value;
-        const email = form.email.value;
-        const userName = form.userName.value;
-        const password = form.password.value;
-        const confirmPassword = form.confirmPassword.value;
+        const formData = {};
+        form.forEach((value, key) => {
+            formData[key] = value;
+        });
 
-        console.log('First Name:', firstName);
-        console.log('Email:', email);
-        console.log('User:', userName);
-        console.log('Password:', password);
-        console.log('Confirm Password:', confirmPassword);
-        console.log('Date:', date);
-    }
+        const mistake = validateFields(formData);
+        setMistakes(mistake);
+    };
 
     return (
         <div className='containerRegister'>
             <p className='logo'>No Country Tube</p>
             <form className='formRegister' onSubmit={handleSubmit}>
-                <input className='inputRegister' type="text" placeholder='First name' id='firstName' name='firstName' required />
-                <input className='inputRegister' type="email" placeholder='Email' id='email' name='email' required />
-                <input className='inputRegister' type="text" placeholder='User name' id='userName' name='userName' required />
-                <input className='inputRegister' type="password" placeholder='Password' id='password' name='password' required />
-                <input className='inputRegister' type="password" placeholder='Confirm password' id='confirmPassword' name='confirmPassword' required />
-                <input className='inputRegister' type="date" id='birthDay' name='birthDay' value={date} onChange={handleDateChange} required />
+                <input className='inputRegister' type="text" placeholder='First name' id='firstName' name='firstName' />
+                {mistakes.firstName && <span className='alertRegister alertFirstNameRegister'>{mistakes.firstName}</span>}
+                <input className='inputRegister' type="email" placeholder='Email' id='email' name='email' />
+                {mistakes.email && <span className='alertRegister alertEmailRegister'>{mistakes.email}</span>}
+                <input className='inputRegister' type="text" placeholder='User name' id='userName' name='userName' />
+                {mistakes.userName && <span className='alertRegister alertUsernameRegister'>{mistakes.userName}</span>}
+                
+                <div className='tooltipPasswordContainer'>
+                    <input className='inputRegister' type="password" placeholder='Password' id='password' name='password' />
+                    {mistakes.password && <span className='alertRegister alertPasswordRegister'>{mistakes.password}</span>}
+                    <span className="tooltipPassword">
+                        <ul className='requirementsPassword'>
+                            <li>Al menos una letra minúscula.</li>
+                            <li>Al menos una letra mayúscula.</li>
+                            <li>Al menos un dígito.</li>
+                            <li>Al menos un carácter especial (por ejemplo, @, #, $, %, &, etc.).</li>
+                            <li>Una longitud mínima de 8 caracteres.</li>
+                        </ul>
+                    </span>
+                </div>
+                
+                <input className='inputRegister' type="password" placeholder='Confirm password' id='confirmPassword' name='confirmPassword' />
+                {mistakes.confirmPassword && <span className='alertRegister alertConfirmPasswordRegister'>{mistakes.confirmPassword}</span>}
+                <input className='inputRegister' type="date" id='birthDay' name='birthDay' value={date} onChange={(event) => setDate(event.target.value)} />
+                {/* {mistakes.birthDay && <span >{mistakes.birthDay}</span>} */}
 
                 <button className='buttonRegister' type='submit'>Regístrate</button>
             </form>
         </div>
-    )
+    );
 }
 
-export default FormRegister
+export default FormRegister;
