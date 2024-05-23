ba6af9d624adde60addb68efd729e00b64b37287
mateok13 <mateoroserok13@gmail.com>
2024-05-23 08:53:04 -0500

    Login and registration pages ready, it would be necessary to use the end points

--- src/components/formLogin/FormLogin.jsx
+++ src/components/formLogin/FormLogin.jsx
@@ -1,40 +1,9 @@
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import useFormLogin from '../../hooks/useFormLogin';
 import './FormLogin.css'
 
 function FormLogin() {
-    const [mistakes, setMistakes] = useState({});
-
-    const validateFields = (form) => {
-        let mistakes = {};
-        let regexUserName = /^[A-Za-z0-9]+$/;
-
-        if (!form.userName.trim()) {
-            mistakes.userName = 'The user name field must not be empty, enter your user name';
-        } else if (!regexUserName.test(form.userName)) {
-            mistakes.userName = 'The user name can only contain letters and numbers';
-        }
-
-        if (!form.password.trim()) {
-            mistakes.password = 'The password field must not be empty, enter your password';
-        }
-
-        return mistakes;
-    }
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-
-        const form = new FormData(event.target);
-
-        const formData = {};
-        form.forEach((value, key) => {
-            formData[key] = value;
-        });
-
-        const mistake = validateFields(formData);
-        setMistakes(mistake);
-    }
+    const { mistakes, handleSubmit } = useFormLogin();
 
     return (
         <div className='containerLogin'>
--- src/components/formRegister/FormRegister.jsx
+++ src/components/formRegister/FormRegister.jsx
@@ -1,64 +1,30 @@
-import { useState } from 'react';
+import { useState, forwardRef } from 'react';
+import DatePicker from "react-datepicker";
+import InputMask from 'react-input-mask';
+import "react-datepicker/dist/react-datepicker.css";
 import './FormRegister.css';
+import useFormRegister from '../../hooks/useFormRegister';
 
 function FormRegister() {
+    const [dateDatePicker, setDateDatePicker] = useState(null);
     const [date, setDate] = useState('');
+    const { mistakes, handleSubmit } = useFormRegister();
 
-    const [mistakes, setMistakes] = useState({});
+    // eslint-disable-next-line react/prop-types
+    const ButtonDate = forwardRef(function CustomInput({ onClick }, ref) {
+        return (
+            <button className='buttonPicker' type="button" onClick={onClick} ref={ref}>📅</button>
+        );
+    });
 
-    const validateFields = (form) => {
-        let mistakes = {};
-        let regexTextOnly = /^[A-Za-záéíóúÁÉÍÓÚüÜñÑ]+(\s[A-Za-záéíóúÁÉÍÓÚüÜñÑ]+)*$/;
-        let regexEmail = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/;
-        let regexUserName = /^[A-Za-z0-9]+$/;
-        let regexPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-        // let regexFecha = /^(19|20)\d{2}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/;
-
-        if (!form.firstName.trim()) {
-            mistakes.firstName = 'The first name field must not be empty, enter your first name';
-        } else if (!regexTextOnly.test(form.firstName)) {
-            mistakes.firstName = 'The first name can only contain letters and spaces (a space between each word)';
+    const handleDateChange = (date) => {
+        setDateDatePicker(date);
+        if (date) {
+            const formatted = `${('0' + date.getDate()).slice(-2)}/${('0' + (date.getMonth() + 1)).slice(-2)}/${date.getFullYear()}`;
+            setDate(formatted);
+        } else {
+            setDate('');
         }
-
-        if (!form.email.trim()) {
-            mistakes.email = 'The email field must not be empty, enter your email';
-          } else if (!regexEmail.test(form.email)) {
-            mistakes.email = 'The email has an invalid format';
-          }
-
-        if (!form.userName.trim()) {
-            mistakes.userName = 'The user name field must not be empty, enter your user name';
-        } else if (!regexUserName.test(form.userName)) {
-            mistakes.userName = 'The user name can only contain letters and numbers';
-        }
-
-        if (!form.password.trim()) {
-            mistakes.password = 'The password field must not be empty, enter your password';
-        } else if(!regexPassword.test(form.password)){
-            mistakes.password = 'The password has an invalid format';
-        }
-
-        if (!form.confirmPassword.trim()) {
-            mistakes.confirmPassword = 'The confirm password field must not be empty, confirm your password';
-        } else if (form.password !== form.confirmPassword) {
-            mistakes.confirmPassword = 'The passwords do not match';
-        }
-
-        return mistakes;
-    }
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-
-        const form = new FormData(event.target);
-
-        const formData = {};
-        form.forEach((value, key) => {
-            formData[key] = value;
-        });
-
-        const mistake = validateFields(formData);
-        setMistakes(mistake);
     };
 
     return (
@@ -71,7 +37,7 @@ function FormRegister() {
                 {mistakes.email && <span className='alertRegister alertEmailRegister'>{mistakes.email}</span>}
                 <input className='inputRegister' type="text" placeholder='User name' id='userName' name='userName' />
                 {mistakes.userName && <span className='alertRegister alertUsernameRegister'>{mistakes.userName}</span>}
-                
+
                 <div className='tooltipPasswordContainer'>
                     <input className='inputRegister' type="password" placeholder='Password' id='password' name='password' />
                     {mistakes.password && <span className='alertRegister alertPasswordRegister'>{mistakes.password}</span>}
@@ -85,11 +51,25 @@ function FormRegister() {
                         </ul>
                     </span>
                 </div>
-                
+
                 <input className='inputRegister' type="password" placeholder='Confirm password' id='confirmPassword' name='confirmPassword' />
                 {mistakes.confirmPassword && <span className='alertRegister alertConfirmPasswordRegister'>{mistakes.confirmPassword}</span>}
-                <input className='inputRegister' type="date" id='birthDay' name='birthDay' value={date} onChange={(event) => setDate(event.target.value)} />
-                {/* {mistakes.birthDay && <span >{mistakes.birthDay}</span>} */}
+                <div className="dateInputContainer">
+                    <InputMask
+                        mask="99/99/9999"
+                        value={date}
+                        onChange={(e) => setDate(e.target.value)}
+                    >
+                        {(inputProps) => <input {...inputProps} className="inputRegister" placeholder="Birthdate (dd/mm/yyyy)" name="birthDate" />}
+                    </InputMask>
+
+                    <DatePicker
+                        selected={dateDatePicker}
+                        onChange={handleDateChange}
+                        customInput={<ButtonDate />}
+                    />
+                </div>
+                {mistakes.birthDate && <span className='alertRegister alertBirthDateRegister'>{mistakes.birthDate}</span>}
 
                 <button className='buttonRegister' type='submit'>Regístrate</button>
             </form>
