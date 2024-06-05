1377917fcbe7ca8a8d594ab2473c42a3769138eb
Nico <nicolas2289h@gmail.com>
2024-06-05 15:40:07 -0300

    ListAllVideos styles modified

--- frontend/src/components/listAllVideos/ListAllVideos.jsx
+++ frontend/src/components/listAllVideos/ListAllVideos.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import VideoCard from "../videoCard/VideoCard";
 import ejemploListadoVideos from "./ejemploListadoVideos.json";
 import './ListAllVideos.css'
@@ -8,6 +9,7 @@ const ListAllVideos = () => {
 
   return (
     <div className="absolute m-240 m-250 pb-4">
+      <Link to='/upload-video' className="ms-5 position-absolute text-white">Link Formulario de video</Link>
       <div className="container mt-5 d-flex justify-content-center align-items-start flex-wrap gap-4 min-vh-100">
         {listVideos.map((item) => (
           <VideoCard key={item.id} item={item} />
