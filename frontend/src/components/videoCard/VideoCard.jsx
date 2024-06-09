df64159321d4e6005f75557bddf520d59ebb7893
Nico <nicolas2289h@gmail.com>
2024-06-08 23:00:01 -0300

    Get method added for getting all videos

--- frontend/src/components/videoCard/VideoCard.jsx
+++ frontend/src/components/videoCard/VideoCard.jsx
@@ -4,14 +4,14 @@ import { Dropdown } from 'react-bootstrap';
 import { useNavigate } from "react-router-dom";
 
 const VideoCard = ({ item }) => {
-  const { id, title, username, thumbnail, views, duration } = item;
+  const { id, title, nameUser, miniature, duration } = item;
   const navigate = useNavigate()
 
   return (
     <div className="text-white card-width card-border">
       <div className="position-relative cursor-pointer" onClick={() => navigate(`/watch-video/${id}`)}>
         {/* <video src=""></video> */}
-        <img src={thumbnail} className="card-img-top" alt={`Imagen ${item.title}`} />
+        <img src={miniature} className="card-img-top card-img rounded" alt={`Imagen ${item.title}`} />
         <span className="position-absolute end-0 bottom-0 z-3 text-white text-center rounded-1 px-1 m-2 span-duration">{duration}</span>
       </div>
       <div className="pb-0 pt-2 ps-1">
@@ -30,8 +30,8 @@ const VideoCard = ({ item }) => {
           </Dropdown>
         </div>
         <div className="d-flex justify-content-between flex-column">
-          <p className="text-gray width-p my-0 text-ellipsis cursor-pointer" onClick={() => navigate(`/list-videos/${username}`)}>{username}</p>
-          <p className="text-gray"><i className="text-green bi bi-eye"></i> {views} Vistas <span className="fw-bold">·</span> hace 2 horas</p>
+          <p className="text-gray width-p my-0 text-ellipsis cursor-pointer" onClick={() => navigate(`/list-videos/${nameUser}`)}>{nameUser}</p>
+          <p className="text-gray"><i className="text-green bi bi-eye"></i> Vistas <span className="fw-bold">·</span> hace 2 horas</p>
         </div>
       </div>
     </div>
@@ -40,14 +40,15 @@ const VideoCard = ({ item }) => {
 
 VideoCard.propTypes = {
   item: PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    username: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    nameUser: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
+    // video: PropTypes.string.isRequired,
     // description: PropTypes.string.isRequired,
-    thumbnail: PropTypes.string.isRequired,
+    miniature: PropTypes.string.isRequired,
     // video: PropTypes.string.isRequired,
     // comments: PropTypes.string.isRequired,
-    views: PropTypes.number.isRequired,
+    // views: PropTypes.number.isRequired,
     // likes: PropTypes.number.isRequired,
     // dislikes: PropTypes.number.isRequired,
     duration: PropTypes.string.isRequired,
