6d9ea6933970a8b8ca4d771d40f33a69fe68a5e4
Nico <nicolas2289h@gmail.com>
2024-06-08 23:36:29 -0300

    position absolute modified in ListAllvideos container

--- frontend/src/components/listAllVideos/ListAllVideos.jsx
+++ frontend/src/components/listAllVideos/ListAllVideos.jsx
@@ -11,7 +11,7 @@ const ListAllVideos = () => {
   const [listVideos, setListVideos] = useState([]);
   const [limit] = useState(9);
   const [offset, setOffset] = useState(0);
-  const totalVideos = 15; // FALTA EL TOTAL DE VIDEOS
+  const totalVideos = 24; // FALTA EL TOTAL DE VIDEOS
   const [isLoading, setIsLoading] = useState(true);
   const spinnerVideo = (
     <div className="d-flex justify-content-center align-items-center gap-2 px-10">
@@ -57,8 +57,8 @@ const ListAllVideos = () => {
   }, [isLoading, offset, limit, totalVideos]);
 
   return (
-    <div className="absolute m-240 m-250 pb-4">
-      <Link to='/upload-video' className="ms-5 position-absolute text-white">Link Formulario de video</Link>
+    <div className="m-240 m-250 pb-4"> {/* se quito absolute*/}
+      <Link to='/upload-video' className="container mt-5 d-flex justify-content-center align-items-start flex-wrap text-white">Link Formulario de video</Link>
       <div className="container mt-5 d-flex justify-content-center align-items-start flex-wrap gap-4 min-vh-100">
         {
           // isLoading && listVideos.length == 0 ? <Spinner className="color-spinner"></Spinner> :
@@ -66,7 +66,7 @@ const ListAllVideos = () => {
             <VideoCard key={item.id} item={item} />
           ))
         }
-        {(isLoading && listVideos.length > 1) && spinnerVideo}
+        {(isLoading) && spinnerVideo}
       </div>
     </div>
   );
